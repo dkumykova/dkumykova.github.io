b0e11f154722f4710bedf490530862a7bdabac27
Diana Kumykova <dkumykova@wpi.edu>
2019-09-22 21:22:25 -0400

    added proj bases

--- src/pages/workPage.vue
+++ src/pages/workPage.vue
@@ -2,9 +2,13 @@
  
  <div id="workPage">
     <!-- <p>testing testing</p> -->
-    <projBox></projBox>
-    <projBox></projBox>
-    <projBox></projBox>
+    <projBox title="Software Developer Intern"  subtitle="Tyler Technologies" description="testing testing"></projBox>
+    <projBox title="Software Developer" subtitle="UXDM Lab" description="stuff"></projBox>
+    <projBox title="Lead Programmer + Audio" subtitle="Call of Karen" description="yay"></projBox>
+    <projBox title="Forest Fighters" subtitle="Game Jam 2018" description="link to video"></projBox>
+    <projBox title="VR Demo" subtitle="Global Game Jam 2018" description="link to video?"></projBox>
+
+
  </div>
   
 </template>
@@ -27,10 +31,5 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-a {
-  color: #42b983;
-}
-/* body{
-  margin-top: 100px;
-} */
+@import '../styles/style.css';
 </style>
