e516efb87525ffe273100bc8583a4b1cecae5a52
Diana <dkumykova@wpi.edu>
2020-01-16 10:44:52 -0500

    yikes

--- deploy.sh
+++ deploy.sh
@@ -19,6 +19,6 @@ git add -A
 git commit -m 'deploy'
 
 #deploying to username.github.io
-git push -f git@github.com:dkumykova/dkumykova.github.io.git master
+git push -f https://github.com/dkumykova/dkumykova.github.io.git master
 
 cd -
