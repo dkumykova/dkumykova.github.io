51e8aa435aeea8940fab2103ba2023a827d895fd
DK <dkumykova@gmail.com>
2020-03-19 17:04:26 -0400

    pretty good

--- routes/index.js
+++ routes/index.js
@@ -16,26 +16,20 @@ router.get('/karen', function(req, res, next) {
   'starting with her home. Play through minigames of what a suburban mom would do as her house slowly' + 
   ' descends into chaos.', 
       role:'Lead Gameplay and Audio Programmer', tools:'Unreal Engine 4, Reaper',
-    picture1: '../../images/meatloaf.gif',
-    picture2: '../../images/books.gif',
-  trailer: 'https://www.youtube.com/embed/TGbZYUslGhQ'});
+  trailer: 'https://www.youtube.com/embed/TGbZYUslGhQ', image: 'n'});
 });
 
 router.get('/fighters', function(req, res, next) {
-  res.render('game', {title: 'Forest Fighters', synopsis: ' Play as birch,'
+  res.render('game', {title: 'Forest Fighters', synopsis: ' Play as birch, '
   + 'oak, or any other of your favorite muscley trees and fight to the death! Forest Fighters was created as a local game jam project with a team of 4 people in 2018. ', 
       role:'Audio and Programmer', tools:'Gamemaker, Reaper',
-    picture1: '../../images/meatloaf.gif',
-    picture2: '../../images/books.gif',
-    trailer: '../../images/books.gif'});
+    trailer: '../../images/ff_show.jpg', image:'y'});
 });
 
 router.get('/website', function(req, res, next) {
   res.render('game', {title: 'Personal Website', synopsis: ' My personal website and long-standing project, built and rebuilt from scratch over the course of 1.5 years.', 
       role:' Programmer', tools:'EJS, Bootstrap, HTML/CSS, Javascript',
-    picture1: '../../images/meatloaf.gif',
-    picture2: '../../images/books.gif',
-    trailer: '../../images/books.gif'});
+    trailer: '../../images/web_before_after.png', image:'y'});
 });
 
 router.get('/fatal', function(req, res, next) {
@@ -44,37 +38,31 @@ router.get('/fatal', function(req, res, next) {
   + 'abilities. Set in the forbidden block of memory from which no language can allocate, players fight to death in a Tekken-like 1v1 arena, and take out '
  + 'aggression against their most hated language. You can play as Python, C, C++, Lisp, or Java, and prove once and for all which is the best programming language!', 
       role:' Programmer and Audio', tools:'Dragonfly Engine, C++', 
-    picture1: '../../images/meatloaf.gif',
-    picture2: '../../images/books.gif',
-    trailer: 'https://www.youtube.com/embed/tGfXXczZ4Js'});
+    trailer: 'https://www.youtube.com/embed/tGfXXczZ4Js', image:'n'});
 });
 
 router.get('/boids', function(req, res, next) {
   res.render('game', {title: 'Birds and Boids', synopsis: ' Birds and Boids is designed to be a tranquil, relaxing experience that allows users to interact '
   + 'with boids as they float across the screen and bring music where they go. Uses the Leap Motion for more immersive boid-to-human interaction.', 
       role:' Programmer and Audio', tools:'Unity, Leap Motion',
-    picture1: '../../images/meatloaf.gif',
-    picture2: '../../images/books.gif',
-    trailer: 'https://www.youtube.com/embed/4rx7Z99PztU'});
+    trailer: 'https://www.youtube.com/embed/4rx7Z99PztU', image:'n'});
 });
 
   router.get('/dragonfly', function(req, res, next) {
-    res.render('game', {title: 'Dragonfly Engine', synopsis: ' Dragonfly is a fully functional ASCII-based game engine in made in C++, utilizing the SFML library for grahphics'
+    res.render('game', {title: 'Dragonfly Engine', synopsis: ' Dragonfly is a fully functional ASCII-based game engine in made in C++, utilizing the SFML library for grahphics '
     + 'and audio. It supports game objects, 2D and 2.5D physics, user-uploaded ASCII styles and custom audio.', 
         role:' Programmer', tools:'C++',
-      picture1: '../../images/meatloaf.gif',
-      picture2: '../../images/books.gif',
-      trailer: '#'});
+      trailer: '../../images/dragonfly_show.jpg', image:'y'});
   });
 
-    router.get('/doll', function(req, res, next) {
-      res.render('game', {title: 'PCG Doll Generator', synopsis: ' This nifty tool utilizes paramterized procedural '
-      + 'generation to create a customizable Russian Doll set! It was completed during the last half of Procjam 2019.', 
-          role:' Programmer', tools:'NodeJS, Javascript',
-        picture1: '../../images/meatloaf.gif',
-        picture2: '../../images/books.gif',
-        trailer: '#'});
-    });
+    // router.get('/doll', function(req, res, next) {
+    //   res.render('game', {title: 'PCG Doll Generator', synopsis: ' This nifty tool utilizes paramterized procedural '
+    //   + 'generation to create a customizable Russian Doll set! It was completed during the last half of Procjam 2019.', 
+    //       role:' Programmer', tools:'NodeJS, Javascript',
+    //     picture1: '../../images/meatloaf.gif',
+    //     picture2: '../../images/books.gif',
+    //     trailer: '#', image:'y'});
+    // });
 
 
 module.exports = router;
