928fd40feee8ef1e558a897be6b61e9bbbc13731
DK <dkumykova@gmail.com>
2020-05-24 17:28:54 -0400

    much better

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var gameInfo = require('../public/js/gameInfoText');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -15,55 +16,45 @@ router.get('/resume', function(req, res, next) {
 });
 
 router.get('/karen', function(req, res, next) {
-  res.render('game', {title: 'Call Of Karen', synopsis: ' Karen is a typical 1950s suburban housewife, trapped in an unhappy marriage and taking care of an '
-   + 'ungrateful child. If that wasn’t bad enough, the stars are right and Cthulhu is descending upon the world,' +  
-  'starting with her home. Play through minigames of what a suburban mom would do as her house slowly' + 
-  ' descends into chaos.', 
-      role:'Lead Gameplay and Audio Programmer', tools:'Unreal Engine 4, Reaper', images: ["../../images/karen_breakfast.jpg", "../../images/karen_cleaning.jpg", "../../images/karen_dinner.jpg", "../../images/karen_spook.jpg"],
-  trailer: 'https://www.youtube.com/embed/BB3jwdJzrFQ', isTrailer: 'y', contrib: 'I did lots of tjhings it was cool'});
+  res.render('game', {title: 'Call Of Karen', synopsis: gameInfo.description.karen,
+      role:'Lead Gameplay and Audio Programmer', tools:'Unreal Engine 4, Reaper', images: ["../../images/karen_breakfast.jpg", "../../images/karen_cleaning.jpg", "../../images/karen_dinner.jpg", "../../images/karen_spook.jpg", "../../images/books.gif"],
+  trailer: 'https://www.youtube.com/embed/BB3jwdJzrFQ', isTrailer: 'y', contrib: gameInfo.contributions.karen, steam: 'y', canPlay: 'n'});
 });
 
 router.get('/fighters', function(req, res, next) {
-  res.render('game', {title: 'Forest Fighters', synopsis: ' Play as birch, '
-  + 'oak, or any other of your favorite muscley trees and fight to the death! Forest Fighters was created as a local game jam project with a team of 4 people in 2018. ', 
-      role:'Audio and Programmer', tools:'Gamemaker, Reaper', images: [],
-    trailer: '../../images/ff_show.jpg', isTrailer:'n', contrib: 'I did lots of tjhings it was cool'});
+  res.render('game', {title: 'Forest Fighters', synopsis: gameInfo.description.fighters,
+      role:'Audio and Programmer', tools:'Gamemaker, Reaper', images: ["../../images/ff_title_screen.jpg","../../images/forest_fighters.png", "../../images/ff_kick.png", "../../images/ff_end.jpg"],
+    trailer: '', isTrailer:'n', contrib: gameInfo.contributions.fighters, steam: 'n', canPlay: 'n'});
 });
 
 router.get('/website', function(req, res, next) {
-  res.render('game', {title: 'Personal Website', synopsis: ' My personal website and long-standing project, built and rebuilt from scratch over the course of 1.5 years.', 
-      role:' Programmer', tools:'EJS, Bootstrap, HTML/CSS, Javascript', images: [],
-    trailer: '', isTrailer:'n', contrib: 'I did lots of tjhings it was cool'});
+  res.render('game', {title: 'Personal Website', synopsis: gameInfo.description.website,
+      role:' Programmer', tools:'EJS, Bootstrap, HTML/CSS, Javascript', images: ["../../images/old_website.png"],
+    trailer: '', isTrailer:'n', contrib: gameInfo.contributions.website, steam: 'n', canPlay: 'n'});
 });
 
 router.get('/fatal', function(req, res, next) {
-  res.render('game', {title: 'Fatal Error', synopsis: 'In this game you and your opponent ' 
-  + 'fight as one of the 5 most well-known programming languages, and are pitted against one another in order to show off your language’s most outstanding ' 
-  + 'abilities. Set in the forbidden block of memory from which no language can allocate, players fight to death in a Tekken-like 1v1 arena, and take out '
- + 'aggression against their most hated language. You can play as Python, C, C++, Lisp, or Java, and prove once and for all which is the best programming language!', 
+  res.render('game', {title: 'Fatal Error', synopsis:  gameInfo.description.fatal,
       role:' Programmer and Audio', tools:'Dragonfly Engine, C++', images: [],
-    trailer: 'https://www.youtube.com/embed/tGfXXczZ4Js', isTrailer:'y', contrib: 'I did lots of tjhings it was cool'});
+    trailer: 'https://www.youtube.com/embed/tGfXXczZ4Js', isTrailer:'y', contrib: gameInfo.contributions.fatal, steam: 'n', canPlay: 'n'});
 });
 
 router.get('/boids', function(req, res, next) {
-  res.render('game', {title: 'Birds and Boids', synopsis: ' Birds and Boids is designed to be a tranquil, relaxing experience that allows users to interact '
-  + 'with boids as they float across the screen and bring music where they go. Uses the Leap Motion for more immersive boid-to-human interaction.', 
+  res.render('game', {title: 'Birds and Boids', synopsis: gameInfo.description.boids,
       role:' Programmer and Audio', tools:'Unity, Leap Motion', images: [],
-    trailer: 'https://www.youtube.com/embed/4rx7Z99PztU', isTrailer:'y', contrib: 'I did lots of tjhings it was cool'});
+    trailer: 'https://www.youtube.com/embed/4rx7Z99PztU', isTrailer:'y', contrib: gameInfo.contributions.boids, steam: 'n', canPlay: 'n'});
 });
 
 router.get('/dragonfly', function(req, res, next) {
-  res.render('game', {title: 'Dragonfly Engine', synopsis: ' Dragonfly is a fully functional ASCII-based game engine in made in C++, utilizing the SFML library for grahphics '
-  + 'and audio. It supports game objects, 2D and 2.5D physics, user-uploaded ASCII styles and custom audio.', 
-      role:' Programmer', tools:'C++', images: ["../../images/saucer_shoot_play.png", "../../images/state_machine_smaple.png",],
-    trailer: '', isTrailer:'n', contrib: 'I did lots of tjhings it was cool'});
+  res.render('game', {title: 'Dragonfly Engine', synopsis: gameInfo.description.dragonfly,
+      role:' Programmer', tools:'C++', images: ["../../images/state_machine_sample.png", "../../images/saucer_shoot_play.png"],
+    trailer: '', isTrailer:'n', contrib: gameInfo.contributions.dragonfly, steam: 'n', canPlay: 'n'});
 });
 
 router.get('/coronagotchi', function(req, res, next) {
-  res.render('game', {title: 'Coronagotchi', synopsis: ' Coronagotchi allows players to live out the nostalgia of older handheld games while either helping '
-  + 'or harming their little coronavirus pet in this dark humor twist on the classic Tamagotchi device.', 
-      role:' Programmer and Audio', tools:'Perlenspiel Engine (Javascript)', images : ["../../images/dragonfly_show.jpg", "../../images/web_before_after.png", "../../images/ff_show.jpg"],
-    trailer: '', isTrailer:'n', contrib: 'I did lots of tjhings it was cool'});
+  res.render('game', {title: 'Coronagotchi', synopsis: gameInfo.description.coronagotchi,
+      role:' Programmer and Audio', tools:'Perlenspiel Engine (Javascript)', images : ["../../images/corona_stats.png", "../../images/corona_breaker.png", "../../images/corona_mem_game.png","../../images/corona_dress.png"],
+    trailer: '', isTrailer:'n', contrib: gameInfo.contributions.coronagotchi, steam: 'n', canPlay: 'y'});
 });
     
 
